4f7bcb674df16fb4085dacc191662d0ad8eb2bce
Ali Samet <ali.samet.ar@gmail.com>
2024-05-19 09:49:39 +0300

    Guncelleme Son

--- giris.php
+++ giris.php
@@ -10,7 +10,12 @@ $password = $_POST['password'];
 // Kullanıcı adı ve şifre kontrolü
 if ($email == $allowed_email && $password == $allowed_password) {
     echo "Giriş başarılı!";
+    header("Refresh: 2; URL=index.html");
+    exit();
 } else {
+    // Hata mesajını ekrana yazdırmadan önce başlıkları gönderin
+    header("Refresh: 2; URL=Giris.html");
     echo "Eposta veya şifre yanlış. Lütfen tekrar deneyin.";
+    exit();
 }
 ?>
